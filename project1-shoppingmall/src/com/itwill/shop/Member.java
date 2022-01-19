4422014245e85366c6fd1cb35ca312eeb75f0411
윤용환 <@61.105.197.217>
2022-01-19 13:40:13 +0900

    두번째커밋

--- project1-shoppingmall/src/com/itwill/shop/Member.java
+++ project1-shoppingmall/src/com/itwill/shop/Member.java
@@ -1,5 +1,23 @@
 package com.itwill.shop;
 
 public class Member {
+	private int no;
 
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public Member(int no) {
+		super();
+		this.no = no;
+	}
+	public Member() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 }
